73c118b2b7b3d127ae2d5a928b7b316bf48ef808
sandipthapa99 <sandipthapa386@gmail.com>
2024-01-06 22:57:13 +0545

    feat: Login page basic layout

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { SvgComponent } from "./assets/svgs";
+import { COLORS, SIZES } from "./constants/theme";
+import {
+  Input,
+  Icon,
+  Stack,
+  Pressable,
+  Center,
+  NativeBaseProvider,
+  FormControl,
+  Button,
+} from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
 SplashScreen.preventAutoHideAsync();
 setTimeout(SplashScreen.hideAsync, 5000);
 
 export default function App() {
+  const [show, setShow] = useState(false);
   const [fontsLoaded] = useFonts({
     "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
   });
 
   const onLayoutRootView = useCallback(async () => {
@@ -21,26 +37,93 @@ export default function App() {
   if (!fontsLoaded) {
     return null;
   }
+
   return (
-    <View style={styles.container}>
-      <SvgComponent />
-      <Text style={styles.text}>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <SvgComponent />
+
+          <Text style={styles.welcomeText}>Welcome back, 👋 </Text>
+          <Text style={styles.subText}>It's good to see you again. </Text>
+          <Stack space={4} w="100%" alignItems="center">
+            <FormControl>
+              <FormControl.Label>Email</FormControl.Label>
+              <Input
+                InputLeftElement={
+                  <Icon
+                    as={<MaterialIcons name="mail" />}
+                    size={5}
+                    ml="2"
+                    color="muted.400"
+                  />
+                }
+                placeholder="username@company.com"
+                style={{
+                  fontFamily: "Poppins-Regular",
+                }}
+              />
+            </FormControl>
+            <Input
+              type={show ? "text" : "password"}
+              InputLeftElement={
+                <Icon
+                  as={<MaterialIcons name="lock" />}
+                  size={5}
+                  ml="2"
+                  color="muted.400"
+                />
+              }
+              style={{ fontFamily: "Poppins-Regular" }}
+              InputRightElement={
+                <Pressable onPress={() => setShow(!show)}>
+                  <Icon
+                    as={
+                      <MaterialIcons
+                        name={show ? "visibility" : "visibility-off"}
+                      />
+                    }
+                    size={5}
+                    mr="2"
+                    color="muted.400"
+                  />
+                </Pressable>
+              }
+              placeholder="Password"
+            />
+          </Stack>
+          <Button
+            size="sm"
+            colorScheme="secondary"
+            width={"100%"}
+            style={{ backgroundColor: COLORS.primary }}
+          >
+            LOGIN
+          </Button>
+          {/* <StatusBar style="auto" /> */}
+        </View>
+      </SafeAreaView>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: SIZES.medium,
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    fontFamily: "Poppins-Bold",
-    fontSize: 30,
+  welcomeText: {
+    fontFamily: "Poppins-Medium",
+    fontSize: 24,
+    color: COLORS.textPrimary,
+    marginTop: 16,
+  },
+  subText: {
+    fontFamily: "Poppins-Regular",
+    fontSize: 12,
+    color: COLORS.textGray,
   },
 });
---
+++ constants/theme.js
@@ -0,0 +1,25 @@
+const COLORS = {
+  primary: "#007FEE",
+  secondary: "#444262",
+  tertiary: "#FF7754",
+
+  textPrimary: "#1E293B",
+  textGray: "#555E63",
+
+  gray: "#83829A",
+  gray2: "#C1C0C8",
+
+  white: "#F3F4F8",
+  lightWhite: "#FAFAFC",
+};
+
+const SIZES = {
+  xSmall: 10,
+  small: 12,
+  medium: 16,
+  large: 20,
+  xLarge: 24,
+  xxLarge: 32,
+};
+
+export { COLORS, SIZES };
